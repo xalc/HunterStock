0530ed5a57a7472ffe4945f5823af6ab7273a7f3
xalc <chao1990li@gmail.com>
2020-09-17 00:50:55 +0800

    add header for and navbar for

--- src/containers/Header/Header.js
+++ src/containers/Header/Header.js
@@ -1,15 +1,17 @@
 import React from 'react';
-import { Navbar, Nav } from 'react-bootstrap'
+import { Navbar, Nav } from 'react-bootstrap';
+import { NavLink, Link } from 'react-router-dom';
+import styles from './Header.module.scss';
 function Header() {
     return (
-      <div className="header">
-        <Navbar bg="dark" expand="lg">
-            <Navbar.Brand href="#home">天择</Navbar.Brand>
+      <div className={styles.header}>
+        <Navbar bg="dark" variant="dark" expand="lg">
+            <Navbar.Brand as= {Link} to="/home" >天择</Navbar.Brand>
             <Navbar.Toggle aria-controls="basic-navbar-nav" />
             <Navbar.Collapse id="basic-navbar-nav">
-                <Nav className="mr-auto">
-                    <Nav.Link href="#home">Home</Nav.Link>
-                    <Nav.Link href="#link">Link</Nav.Link>
+                <Nav className="mr-auto">  
+                    <Nav.Link as= {Link} to="/about">about</Nav.Link>
+                    <Link className="nav-link" to="/">Home</Link>
                 </Nav>
             </Navbar.Collapse>
             
